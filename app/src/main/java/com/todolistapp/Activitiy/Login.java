bf575eb1f52dbca19c76f99ea2d7d36e7ad9b480
AlpYuktug <alperen.altug@gmail.com>
2019-10-17 00:05:59 +0300

    Login

--- app/src/main/java/com/todolistapp/Activitiy/Login.java
+++ app/src/main/java/com/todolistapp/Activitiy/Login.java
@@ -1,14 +1,31 @@
 package com.todolistapp.Activitiy;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
 import com.todolistapp.R;
 
 public class Login extends AppCompatActivity {
+
+    public ImageView imageViewLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        imageViewLogin = findViewById(R.id.imageViewLogin);
+        imageViewLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                Intent intentHome = new Intent(Login.this,Home.class);
+                startActivity(intentHome);
+            }
+        });
     }
 }
